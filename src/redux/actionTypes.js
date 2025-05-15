a95d6c3d2ededdddfe4054f8c191dc4fc8040754
Programcı Ruhu <nrlmnnn532@gmail.com>
2025-05-15 17:08:50 +0400

    actionTypes.js

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,4 +1,3 @@
-// Redux üçün sabit action növləri
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const INCREASE_QUANTITY = 'INCREASE_QUANTITY';
 export const DECREASE_QUANTITY = 'DECREASE_QUANTITY';
